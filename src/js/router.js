4b5cc72270c8510c849c54a384c979f2516b1ee5
ShowMeYourSkil <39274150+ShowMeYourSkil@users.noreply.github.com>
2020-01-28 14:28:20 +0100

    Create router.js

---
+++ src/js/router.js
@@ -0,0 +1,22 @@
+/** @format */
+
+import Router from 'vue-router';
+import Main from '../routes/Main';
+import Discord from '../routes/Discord';
+
+export default new Router({
+  mode: 'history',
+
+  routes: [
+    {
+      path: '/',
+      name: 'Main',
+      component: Main,
+    },
+    {
+      path: '/discord',
+      name: 'Discord',
+      component: Discord,
+    },
+  ],
+});
